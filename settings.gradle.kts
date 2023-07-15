3e32c64e9984ca4cb94ccfeac9cf37c3322e3216
Niklas Widmann <nik@somethingcatchy.net>
2023-07-15 15:30:03 +0200

    outsource most gradle configuration to plugin (#76)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,29 @@
+val mod_name: String by extra
+
 pluginManagement {
     repositories {
+        val env = System.getenv()
+
         gradlePluginPortal()
-        maven {
-            url = uri("https://maven.minecraftforge.net/")
+        maven { url = uri("https://maven.minecraftforge.net/") }
+        maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
+
+        env["LOCAL_MAVEN"]?.let { localMaven ->
+            maven { url = uri(localMaven) }
         }
+
         maven {
-            url = uri("https://repo.spongepowered.org/repository/maven-public/")
+            url = uri("https://maven.pkg.github.com/PssbleTrngle/FlightLib")
+            credentials {
+                username = env["GITHUB_ACTOR"]
+                password = env["GITHUB_TOKEN"]
+            }
+            content {
+                includeGroup("com.possible-triangle")
+            }
         }
     }
 }
 
-rootProject.name = "Create Jetpack"
+rootProject.name = mod_name
 
