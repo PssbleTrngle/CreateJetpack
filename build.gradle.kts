0dd962e0da9bd4e6cc302a0c997b01bdf94664ae
Niklas Widmann <nik@somethingcatchy.net>
2023-09-06 14:50:44 +0200

    update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val elytra_slot_version: String by extra
 val jei_version: String by extra
 
 plugins {
-    id("net.somethingcatchy.gradle") version("0.0.3")
+    id("com.possible-triangle.gradle") version("0.1.0")
 }
 
 withKotlin()
