d36167b3d3c6adc287ef76fdd2fc73743fc41d54
Niklas Widmann <nik@somethingcatchy.net>
2023-09-22 09:45:26 +0200

    update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val elytra_slot_version: String by extra
 val jei_version: String by extra
 
 plugins {
-    id("com.possible-triangle.gradle") version("0.1.0")
+    id("com.possible-triangle.gradle") version("0.1.1")
 }
 
 withKotlin()
@@ -84,9 +84,7 @@ tasks.withType<Jar> {
 }
 
 enablePublishing {
-    repositories {
-        githubPackages(project)
-    }
+    githubPackages()
 }
 
 uploadToCurseforge {
