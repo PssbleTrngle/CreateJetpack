59b8773a0aac07dae48226a5c82c1a2406ebd23a
Niklas Widmann <nik@somethingcatchy.net>
2026-02-10 20:40:05 +0100

    switch to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-val mc_version: String by extra
-val registrate_version: String by extra
-val create_version: String by extra
-val ponder_version: String by extra
-val flywheel_version: String by extra
-val flightlib_version: String by extra
-val curios_version: String by extra
-val caelus_version: String by extra
-val elytra_slot_version: String by extra
-val jei_version: String by extra
-val cold_sweat_version: String by extra
-
 plugins {
     id("com.possible-triangle.neoforge")
 }
@@ -17,7 +5,7 @@ plugins {
 withKotlin()
 
 mod {
-    mods.include("com.possible-triangle:flightlib-neoforge:${flightlib_version}")
+    mods.include(libs.flightlib.neoforge)
 }
 
 neoforge {
@@ -70,30 +58,36 @@ repositories {
 }
 
 dependencies {
-    modImplementation("com.tterrag.registrate:Registrate:${registrate_version}")
-    modImplementation("com.simibubi.create:create-${mc_version}:${create_version}:slim") { isTransitive = false }
-    modImplementation("net.createmod.ponder:Ponder-NeoForge-${mc_version}:${ponder_version}")
-    modCompileOnly("dev.engine-room.flywheel:flywheel-neoforge-api-${mc_version}:${flywheel_version}")
-    modRuntimeOnly("dev.engine-room.flywheel:flywheel-neoforge-${mc_version}:${flywheel_version}")
+    modImplementation(libs.registrate)
 
-    if (!env.isCI) {
-        modRuntimeOnly("mezz.jei:jei-${mc_version}-neoforge:${jei_version}")
-
-        // Only here to test jetpack+elytra combination behaviour
-        modImplementation("top.theillusivec4.curios:curios-neoforge:${curios_version}+${mc_version}")
-        modRuntimeOnly("com.illusivesoulworks.caelus:caelus-neoforge:${caelus_version}+${mc_version}")
-        modRuntimeOnly("maven.modrinth:mSQF1NpT:${elytra_slot_version}")
-        modRuntimeOnly("maven.modrinth:uXhSmPjd:${cold_sweat_version}")
+    modImplementation(
+        variantOf(libs.create) {
+            classifier("slim")
+        },
+    ) {
+        isTransitive = false
     }
 
-    modCompileOnly("com.possible-triangle:flightlib-api:${flightlib_version}")
-    modCompileOnly("com.possible-triangle:flightlib-neoforge-api:${flightlib_version}")
+    modImplementation(libs.ponder)
+    modCompileOnly(libs.flywheel)
+
+    if (!env.isCI) {
+        modRuntimeOnly(libs.jei.neoforge)
+
+        // Only here to test jetpack+elytra combination behaviour
+        modImplementation(libs.curios)
+        modRuntimeOnly(libs.caelus)
+        modRuntimeOnly(pack.modrinth.elytra.slot)
+        modRuntimeOnly(pack.modrinth.cold.sweat)
+    }
+
+    modCompileOnly(libs.flightlib.api)
+    modCompileOnly(libs.flightlib.neoforge.api)
 }
 
 tasks.withType<Jar> {
     exclude("screenshots")
 }
-
 
 upload {
     maven {
